25d3e4067fbe415fe2266d526bd33f9a0d9da934
Sean Chavez <realseanchavez@gmail.com>
2018-12-26 22:45:10 -0800

    async promise chain

--- index.js
+++ index.js
@@ -3,22 +3,10 @@ const fetch = require('node-fetch');
 async function showGithubUser(handle) {
   const url = `https://api.github.com/users/${handle}`;
   const response = await fetch(url);
-  const user = await response.json();
-  console.log(user.name);
-  console.log(user.location);
+  return await response.json();
 }
 
-showGithubUser('mariusschulz');
-
-// const fetch = require('node-fetch');
-
-// async function showGitHubUser(handle) {
-//   const url = `https://api.github.com/users/${handle}`;
-//   const response = await fetch(url);
-//   return await response.json();
-// }
-
-// showGitHubUser('seanchavez').then(user => {
-//   console.log(user.name);
-//   console.log(user.location);
-// });
+showGithubUser('mariusschulz').then(user => {
+  console.log(user.name);
+  console.log(user.location);
+});
