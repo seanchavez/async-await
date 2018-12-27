0fd72599b8bd329577eef6d09c2f397a37bb5107
Sean Chavez <realseanchavez@gmail.com>
2018-12-26 23:51:17 -0800

    error handling'

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const fetch = require('node-fetch');
 
-class GitHubApiClient {
-  async fetchUser(handle) {
-    const url = `https://api.github.com/users/${handle}`;
-    const response = await fetch(url);
-    return await response.json();
-  }
+// class GitHubApiClient {
+//   async fetchUser(handle) {
+//     const url = `https://api.github.com/users/${handle}`;
+//     const response = await fetch(url);
+//     return await response.json();
+//   }
+// }
+
+// (async () => {
+//   const client = new GitHubApiClient();
+//   const user = await client.fetchUser('mariusschulz');
+//   console.log(user.name);
+//   console.log(user.location);
+// })();
+
+async function fetchGithubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+  const response = await fetch(url);
+  const body = await response.json();
+
+  if (response.status !== 200) throw Error(body.message);
+
+  return body;
 }
 
-(async () => {
-  const client = new GitHubApiClient();
-  const user = await client.fetchUser('mariusschulz');
-  console.log(user.name);
-  console.log(user.location);
-})();
+fetchGithubUser('frgnchmghf')
+  .then(user => {
+    console.log(user);
+    //console.log(user.location);
+  })
+  .catch(err => {
+    console.error(`Error: ${err.message}`);
+  });
